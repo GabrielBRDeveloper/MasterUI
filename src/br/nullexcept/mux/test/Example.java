95f98eae99daf03ac0e2030a9d91eec987de09c9
Gabriel <97042217+GabrielBRDeveloper@users.noreply.github.com>
2024-04-13 01:01:48 -0400

    Doc update

--- src/br/nullexcept/mux/test/Example.java
+++ src/br/nullexcept/mux/test/Example.java
@@ -4,12 +4,16 @@ import br.nullexcept.mux.app.Activity;
 import br.nullexcept.mux.app.Application;
 import br.nullexcept.mux.graphics.Bitmap;
 import br.nullexcept.mux.graphics.BitmapFactory;
+import br.nullexcept.mux.graphics.Color;
 import br.nullexcept.mux.graphics.Drawable;
 import br.nullexcept.mux.graphics.drawable.BitmapDrawable;
+import br.nullexcept.mux.graphics.drawable.ColorDrawable;
 import br.nullexcept.mux.utils.Log;
+import br.nullexcept.mux.view.Gravity;
 import br.nullexcept.mux.view.View;
 import br.nullexcept.mux.view.anim.AlphaAnimation;
 import br.nullexcept.mux.view.anim.RotationAnimation;
+import br.nullexcept.mux.widget.TextView;
 
 public class Example extends Activity {
     public static void main(String[] args) {
@@ -19,22 +23,7 @@ public class Example extends Activity {
     @Override
     public void onCreate() {
         super.onCreate();
-        Log.log("Debug", "Hello world | Log");
         setContentView("main");
-        setTitle("Hello world");
-        if (true)return;
-        View panda = findViewById("panda");
-        {
-            RotationAnimation rot = new RotationAnimation(panda, 5000);
-            rot.setLoop(true);
-            rot.play();
-        }
-        {
-            AlphaAnimation alph = new AlphaAnimation(panda, 2000);
-            alph.setAlpha(0,1);
-            alph.setLoop(true);
-            alph.play();
-        }
     }
 
     @Override
